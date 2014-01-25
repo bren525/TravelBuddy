b6c8cef3142bfbcdd72e80e0e2dd73dd075cac09
bren525 <bren525@gmail.com>
2014-01-25 00:06:36 -0500

    Added pre-jquery complete form

--- travelbuddy/routes/index.js
+++ travelbuddy/routes/index.js
@@ -7,13 +7,15 @@ var Trip = models.Trip;
 
 exports.index = function(req, res){
   res.render('index', {
-    title: 'Express',
-    alltrips: global.alltrips,
+    title: 'Travel Buddy',
   });
 };
 exports.addTrip = function(req,res){
 	console.log(req.body);
 	var newTrip = new Trip(req.body.start, 'Providence', 'Boston', new Date('January 24, 2014 12:00:00'))
 	global.alltrips.push(newTrip);
-	res.redirect("/");
+	res.redirect("/list",{
+    title: 'Possible Buddies',
+    alltrips: global.alltrips,
+  });
 };
